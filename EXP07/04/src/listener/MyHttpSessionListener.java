6ca124c08fcde20c903fba3db7fdd5f11941c9b6
092216139 <42989719+092216139@users.noreply.github.com>
2018-10-27 18:32:26 +0800

    Add files via upload

---
+++ EXP07/04/src/listener/MyHttpSessionListener.java
@@ -0,0 +1,40 @@
+package listener;
+ 
+import java.util.ArrayList;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+ 
+import listener.User;
+import listener.SessionUtil;
+/**
+ * HttpSessionListener 监听器
+ *
+ */
+@WebListener
+public class MyHttpSessionListener implements HttpSessionListener{
+	//域对象 number进行数量统计 
+      public int number; 
+
+  	@Override 
+  	//创建 
+  	public void sessionCreated(HttpSessionEvent arg0) { 
+  		number++; 
+  		//在线用户的数量存储到域对象ServletContext的number中 
+  		} 
+  
+  	@Override 
+  	//销毁 
+  	public void sessionDestroyed(HttpSessionEvent arg0) { 
+  		number--; 
+  		arg0.getSession().getServletContext().setAttribute("number", number); 
+  		//list是存储在域对象ServletContext中，用于记录用户的的日志信息 
+  		ArrayList<User> list= 
+  				(ArrayList<User>) arg0.getSession().getServletContext().getAttribute("userList"); 
+  		//根据sessionid删除将要推出的用户信息 
+  		SessionUtil.remove(list,arg0.getSession().getId()); 
+  		arg0.getSession().getServletContext().setAttribute("userList", list); 
+  	} 
+
+ 
+}
