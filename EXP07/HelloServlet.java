9324bd4be33ae8dc42bafe361295453def131c31
092216139 <42989719+092216139@users.noreply.github.com>
2018-10-27 18:20:28 +0800

    Add files via upload

---
+++ EXP07/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("Hello World£¡ I am from Servlet");
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+}
